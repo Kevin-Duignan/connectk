ded20260dee89a82fbca880516871c156efa8c86
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-04-01 22:15:00 +1100

    Add main param

---
+++ connectk.py
@@ -0,0 +1,11 @@
+def main():
+	"""
+	At the start of a game of Connectk, the user inputs:
+		- The number of rows of the game board,
+		- The number of columns of the game board,
+		- The number k of tokens that need to be connected in order to win,
+		- The number of human players,
+		- The number and level of CPU players.
+	:return: None
+	"""
+	pass
