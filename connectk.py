481ba8ad5b7959162cf6db4a043ff7ea76249d53
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-04-01 22:32:16 +1100

    Add helper functions clear_screen and validate_inputs for main

--- connectk.py
+++ connectk.py
@@ -1,3 +1,24 @@
+def clear_screen():
+	# Command for clearing the terminal line
+	os.system("cls" if os.name == "nt" else "clear")
+	
+def validate_input(prompt, valid_inputs):
+    """
+    Repeatedly ask user for input until they enter an input
+    within a set valid of options.
+
+    :param prompt: The prompt to display to the user, string.
+    :param valid_inputs: The range of values to accept, list
+    :return: The user's input, string.
+    """
+    while True:
+		enter = input(prompt)
+		if enter in valid_inputs:
+			return enter
+		else:
+			print("Invalid inputs, please try again.")
+			pass
+
 def main():
 	"""
 	At the start of a game of Connectk, the user inputs:
